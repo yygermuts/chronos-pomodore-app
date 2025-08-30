61cbd9fe675bd4508b1c8b99701561468fc86a96
Yuri Germuts <yygermuts@gmail.com>
2025-08-29 21:19:59 -0300

    starting changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App tipo="Hello, World!" />
+    <App />
   </StrictMode>
 );
